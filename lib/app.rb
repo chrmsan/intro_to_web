c4c35a2191e77247b3fc60bc86462901beb76148
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-10 17:25:29 +0200

    Added params command

--- lib/app.rb
+++ lib/app.rb
@@ -13,8 +13,14 @@ get '/test' do
   "Hello again"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample 
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name] 
   erb(:index)
 end
 
